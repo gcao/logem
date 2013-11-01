bbe326654705cc9f6b169f3e11f267727b50615c
Guoliang Cao <gcao99@gmail.com>
2013-11-01 15:59:34 -0400

    Version bump to 0.4.0

--- lib/logem.rb
+++ lib/logem.rb
@@ -1,1 +1,10 @@
+require 'logem/base_logger'
 require 'logem/logger'
+
+module Logem
+  ERROR = Logger::ERROR
+  WARN  = Logger::WARN
+  INFO  = Logger::INFO
+  DEBUG = Logger::DEBUG
+  TRACE = Logger::TRACE
+end
